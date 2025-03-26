fcbcf2e07079aaf98abb45ab66baf4cca62479d2
chaivertinetik <chaitanya.pandey@vertinetik.com>
2025-03-27 00:50:04 +0530

    Update debug_tree_id.py

--- debug_tree_id.py
+++ debug_tree_id.py
@@ -22,9 +22,5 @@ G.add_node("ash_tree_ids", node_type="data", description="List of 'Tree_ID' for 
 G.add_edge("filter_ash", "ash_tree_ids")
 
 nx.write_graphml(G, "/app/FOXHOLES/Tree_crown_quality.graphml")
-#Print graph information for verification (optional)
-#print(G.nodes(data=True))
-#print(G.edges())
 
-#This part is not required by the prompt, but included for completeness.
 # nx.write_graphml(G, "C:\\Users\\chait\\Projects\\LLM_Geo_GCP\\Hosting env\\Tree_crown_quality\\Tree_crown_quality.graphml")
