d3ab212d872f1c1527d89dc1ee91ce2d0b0d5748
chaivertinetik <chaitanya.pandey@vertinetik.com>
2025-03-28 09:53:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -181,61 +181,61 @@ def long_running_task(job_id, user_task, task_name, data_locations):
         user_task = r"""1) To use a geoJSON file and return all the "Tree_ID" that are ash species ('Predicted Tree Species':'Ash').
         """
         task_name ='Tree_crown_quality'
-        # Create Solution object
-        # solution = Solution(
-        #     task=user_task,
-        #     task_name=task_name,
-        #     save_dir=save_dir,
-        #     data_locations=data_locations,
-        # )
+        Create Solution object
+        solution = Solution(
+            task=user_task,
+            task_name=task_name,
+            save_dir=save_dir,
+            data_locations=data_locations,
+        )
 
         # Generate solution graph
-        # response_for_graph = solution.get_LLM_response_for_graph()
-        # solution.graph_response = response_for_graph
-        # solution.save_solution()
+         response_for_graph = solution.get_LLM_response_for_graph()
+         solution.graph_response = response_for_graph
+         solution.save_solution()
 
-        # file_path = "debug_tree_id.py"
+         file_path = "debug_tree_id.py"
 
         # Read the file content
-        # with open(file_path, "r") as file:
-        #     debugged_code = file.read()
+         with open(file_path, "r") as file:
+             debugged_code = file.read()
         
         # Store the code into solution.code_for_graph
-        # solution.code_for_graph = debugged_code
-        # # print("The debugged code is:",debugged_code)
-        # exec(solution.code_for_graph)
+         solution.code_for_graph = debugged_code
+         # print("The debugged code is:",debugged_code)
+         exec(solution.code_for_graph)
         # Load graph file
-        # solution_graph = solution.load_graph_file()
-        # G = nx.read_graphml(solution.graph_file) 
-        # nt = helper.show_graph(G)
-        # html_name = os.path.join(os.getcwd(), solution.task_name + '.html') 
+         solution_graph = solution.load_graph_file()
+         G = nx.read_graphml(solution.graph_file) 
+         nt = helper.show_graph(G)
+         html_name = os.path.join(os.getcwd(), solution.task_name + '.html') 
 
         # Generate operations
-        # operations = solution.get_LLM_responses_for_operations(review=False)
-        # solution.save_solution()
-        # all_operation_code_str = '\n'.join([operation['operation_code'] for operation in operations])
+         operations = solution.get_LLM_responses_for_operations(review=False)
+         solution.save_solution()
+         all_operation_code_str = '\n'.join([operation['operation_code'] for operation in operations])
 
-        # # Generate assembly code
-        # assembly_LLM_response = solution.get_LLM_assembly_response(review=False)
-        # solution.assembly_LLM_response = assembly_LLM_response
-        # solution.save_solution()
+         # Generate assembly code
+         assembly_LLM_response = solution.get_LLM_assembly_response(review=False)
+         solution.assembly_LLM_response = assembly_LLM_response
+         solution.save_solution()
 
-        # # Run the generated code
-        # model = GenerativeModel("gemini-1.5-flash-002")
-        # for attempt in range(10):
-        #     try: 
-        #         response = model.generate_content(solution.assembly_prompt)
-        #         break
-        #     except ResourceExhausted: 
-        #         if attempt<10:
-        #             time.sleep(10)
-        #         else:
-        #             raise
-        # # response = model.generate_content(solution.assembly_prompt)
-        # code_for_assembly = helper.extract_code(response.text)
+         # Run the generated code
+         model = GenerativeModel("gemini-1.5-flash-002")
+         for attempt in range(10):
+             try: 
+                 response = model.generate_content(solution.assembly_prompt)
+                 break
+             except ResourceExhausted: 
+                 if attempt<10:
+                     time.sleep(10)
+                 else:
+                     raise
+        # response = model.generate_content(solution.assembly_prompt)
+        code_for_assembly = helper.extract_code(response.text)
 
-        # # Combine all code
-        # all_code = all_operation_code_str + '\n' + code_for_assembly + '\n' + 'assembely_solution()'
+        # Combine all code
+        all_code = all_operation_code_str + '\n' + code_for_assembly + '\n' + 'assembely_solution()'
         with open('all_code_id.py', 'r') as file:
             all_code = file.read()
             
